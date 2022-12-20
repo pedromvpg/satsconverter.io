ca2cd077bdf869dd84c9947ef3a4702e6922bbe0
Pedro <pedromvprg@gmail.com>
2022-12-20 18:25:57 +0000

    Change to coingecko API, add CNY, JPY, CAD, RUB, CHF, and flag emojis

--- js/script.js
+++ js/script.js
@@ -9,14 +9,39 @@ $( document ).ready(function() {
 		currency,
 		btc_max_stock = 21000000
 
-	$.getJSON( "https://api.coindesk.com/v1/bpi/currentprice.json", function( data ) {
 
+
+	var apiURLcoindesk = "https://api.coindesk.com/v1/bpi/currentprice.json";
+	var apiURLcoingecko = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd%2Ceur%2Cgbp%2Ccny%2Cjpy%2Ccad%2Cchf%2Crub%2Ctry";
+
+	$.getJSON( apiURLcoingecko, function( data ) {
+
+
+		// coindesk API
+		/*
 		var RateToBTC = {
 			sat: 100000000,
 			btc: 1,
 			usd: data.bpi.USD.rate_float,
 			eur: data.bpi.EUR.rate_float,
 			gbp: data.bpi.GBP.rate_float
+		};
+		*/
+
+		// coingecko API
+		// {"bitcoin":{"usd":16828.13,"eur":15849.27,"gbp":13858.69,"cny":117156,"jpy":2208209,"cad":22929,"chf":15590.78,"rub":1189749,"try":313990}}
+		var RateToBTC = {
+			sat: 100000000,
+			btc: 1,
+			usd: data.bitcoin.usd,
+			eur: data.bitcoin.eur,
+			gbp: data.bitcoin.gbp,
+			cny: data.bitcoin.cny,
+			jpy: data.bitcoin.jpy,
+			cad: data.bitcoin.cad,
+			rub: data.bitcoin.rub,
+			chf: data.bitcoin.chf,
+			try: data.bitcoin.try
 		};
 
 		calcConversion();
@@ -30,6 +55,23 @@ $( document ).ready(function() {
 
 		var satPerGBP = 1/(RateToBTC["gbp"]/1)*100000000;
 		$("#satsgbp").text( satPerGBP.toFixed(0) );
+
+		var satPerCNY = 1/(RateToBTC["cny"]/1)*100000000;
+		$("#satscny").text( satPerCNY.toFixed(0) );
+
+		var satPerJPY = 1/(RateToBTC["jpy"]/1)*100000000;
+		$("#satsjpy").text( satPerJPY.toFixed(0) );
+
+		var satPerCAD = 1/(RateToBTC["cad"]/1)*100000000;
+		$("#satscad").text( satPerCAD.toFixed(0) );
+
+		var satPerRUB = 1/(RateToBTC["rub"]/1)*100000000;
+		$("#satsrub").text( satPerRUB.toFixed(0) );
+
+		var satPerCHF= 1/(RateToBTC["chf"]/1)*100000000;
+		$("#satschf").text( satPerRUB.toFixed(0) );
+
+
 
 
 
@@ -55,7 +97,7 @@ $( document ).ready(function() {
 			else if(source_currency == "btc"){
 				btc_input_value = btc_input_value;
 			}
-			else if(source_currency == "usd" || source_currency == "eur" || source_currency == "gbp"){
+			else if(source_currency == "usd" || source_currency == "eur" || source_currency == "gbp" || source_currency == "cny" || source_currency == "jpy" || source_currency == "cad" || source_currency == "rub" || source_currency == "chf" ){
 				console.log(RateToBTC[source_currency])
 				$btc_input.val( parseFloat( parseFloat(source_val) / parseFloat(RateToBTC[source_currency]) ).toFixed(8) );
 			}
@@ -68,7 +110,7 @@ $( document ).ready(function() {
 				currency = $(this).data("currency");
 				$(this).val( RateToBTC[currency] * btc_input_value );
 
-				(( RateToBTC['sat'] * btc_input_value ) == 1) ? $("#sats-label").text('sat') : $("#sats-label").text('sats');
+				(( RateToBTC['sat'] * btc_input_value ) == 1) ? $("#sats-label").text('⚪️ sat') : $("#sats-label").text('⚪️ sats');
 			})
 
 		};
